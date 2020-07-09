d558fed6404d69dd96db14053e44106166eede37
Gabriel Brown <gabriel.brown@rallyhealth.com>
2020-07-09 15:34:54 -0500

    incorporate ultrasonic sensor

--- gorniebot.py
+++ gorniebot.py
@@ -11,52 +11,55 @@ import time
 
 BP = brickpi3.BrickPi3()
 
-BP.set_sensor_type(BP.PORT_1, BP.SENSOR_TYPE.TOUCH) # Configure for a touch sensor)
+BP.set_sensor_type(BP.PORT_1, BP.SENSOR_TYPE.TOUCH) # Configure for a touch sensor
+BP.set_sensor_type(BP.PORT_2, BP.SENSOR_TYPE.NXT_ULTRASONIC) # Configure for ultrasonic sensor (measures distance)
+
 
 try:
     print("Press touch sensor on port 1 to run motors")
-    value = 0
+    touch_input = 0 # value received from touch sensor, indicates how far sensor is pressed
+    ultrasonic_input = 0 # value received from ultrasonic senso, indicates how far away objects are from the sensor
 
     # Wait for touch sensor to be pressed | GB: not sure this is needed, try removing and see what breaks
-    while not value:
+    while not touch_input:
         try:
-            value = BP.get_sensor(BP.PORT_1)
+            touch_input = BP.get_sensor(BP.PORT_1)
         except brickpi3.SensorError:
             pass
     
-    #speed = 0
-    #adder = 1
     speed = 50
     while True:
-        # BP.get_sensor retrieves a sensor value.
-        # BP.PORT_1 specifies that we are looking for the value of sensor port 1.
-        # BP.get_sensor returns the sensor value.
-        try:
-            value = BP.get_sensor(BP.PORT_1)
+        # Get input from ultrasonic sensor
+         try:
+            value = BP.get_sensor(BP.PORT_2)
+            print(value)                      # print the distance in CM
         except brickpi3.SensorError as error:
             print(error)
-            value = 0
+
+        # Get input from touch sensor
+        #
+        # BP.get_sensor retrieves a sensor value.
+        # BP.PORT_1 specifies that we are looking for the value of sensor port 1.
+        try:
+            touch_input = BP.get_sensor(BP.PORT_1)
+        except brickpi3.SensorError as error:
+            print(error)
+            touch_input = 0
         
-        if not value:
-            speed = 0
+        if not touch_input:
+            speed = 0   # Stop motor if touch sensor is no longer being pressed
         else:
             speed = 50    
-        # if value:                             # if the touch sensor is pressed
-        #     if speed <= -100 or speed >= 100: # if speed reached 100, start ramping down. If speed reached -100, start ramping up.
-        #         adder = -adder
-        #     speed += adder
-        # else:                                 # else the touch sensor is not pressed or not configured, so set the speed to 0
-        #     speed = 0
-        #     adder = 1
         
         # Set the motor speed for all four motors
         BP.set_motor_power(BP.PORT_A + BP.PORT_B + BP.PORT_C + BP.PORT_D, speed)
         
-        try:
-            # Each of the following BP.get_motor_encoder functions returns the encoder value (what we want to display).
-            print("Encoder A: %6d  B: %6d  C: %6d  D: %6d" % (BP.get_motor_encoder(BP.PORT_A), BP.get_motor_encoder(BP.PORT_B), BP.get_motor_encoder(BP.PORT_C), BP.get_motor_encoder(BP.PORT_D)))
-        except IOError as error:
-            print(error)    
+        # try:
+        #     # Each of the following BP.get_motor_encoder functions returns the encoder touch_input (what we want to display).
+        #     #print("Encoder A: %6d  B: %6d  C: %6d  D: %6d" % (BP.get_motor_encoder(BP.PORT_A), BP.get_motor_encoder(BP.PORT_B), BP.get_motor_encoder(BP.PORT_C), BP.get_motor_encoder(BP.PORT_D)))
+        #     print()
+        # except IOError as error:
+        #     print(error)    
         
         time.sleep(0.02)  # delay for 0.02 seconds (20ms) to reduce the Raspberry Pi CPU load.
 
