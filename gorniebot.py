9446da8c463534d09080d986d09c014b3986ada6
Gabriel Brown <gabriel.brown@rallyhealth.com>
2020-07-09 14:50:28 -0500

    Gorniebot placeholder

---
+++ gorniebot.py
@@ -0,0 +1,7 @@
+# My first attempt at writing a custom script for a BrickPi3 robot. 
+# Currently, my plan for the "Gorniebot" is to make a bot that is 
+# set up with a launcher similar to another project, which will roam
+# around and shoot anything that moves.
+
+import brickpi3
+
